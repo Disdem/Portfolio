fee2f8dc240efb62e1193aee23fc83a6138c7df7
Disdem <zap1051@amerike.edu.mx>
2024-11-28 08:31:14 -0600

    commit
    todo bien

---
+++ .svelte-kit/output/server/manifest.js
@@ -0,0 +1,34 @@
+export const manifest = (() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["assets/particles.json","favicon.png","images/Yop-Photoroom.jpg"]),
+	mimeTypes: {".json":"application/json",".png":"image/png",".jpg":"image/jpeg"},
+	_: {
+		client: {"start":"_app/immutable/entry/start.83ccc31b.js","app":"_app/immutable/entry/app.694208b0.js","imports":["_app/immutable/entry/start.83ccc31b.js","_app/immutable/chunks/scheduler.c358b412.js","_app/immutable/chunks/singletons.7197bd47.js","_app/immutable/entry/app.694208b0.js","_app/immutable/chunks/scheduler.c358b412.js","_app/immutable/chunks/index.204ce088.js"],"stylesheets":[],"fonts":[]},
+		nodes: [
+			__memo(() => import('./nodes/0.js')),
+			__memo(() => import('./nodes/1.js')),
+			__memo(() => import('./nodes/2.js'))
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+}
+})();
