2401776613f4424c856404188c3f61286540ab0b
Disdem <zap1051@amerike.edu.mx>
2024-11-28 14:14:01 -0600

    si

--- docs/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.0e24e7b8.js","_app/immutable/chunks/scheduler.c358b412.js","_app/immutable/chunks/index.204ce088.js","_app/immutable/chunks/each.e59479a4.js"];
-export const stylesheets = ["_app/immutable/assets/0.aed99808.css"];
-export const fonts = [];
--- docs/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.3be3def7.js","_app/immutable/chunks/scheduler.c358b412.js","_app/immutable/chunks/index.204ce088.js","_app/immutable/chunks/singletons.1130935f.js"];
-export const stylesheets = [];
-export const fonts = [];
